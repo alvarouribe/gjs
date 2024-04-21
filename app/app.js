3e1036584f9df2fd3f28363055fb03bd0f5dabf1
Alvaro Uribe <iam@alvarouribe.cl>
2024-04-22 11:51:28 +1200

    tailwind set

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'gjs/config/environment';
+import 'gjs/app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
